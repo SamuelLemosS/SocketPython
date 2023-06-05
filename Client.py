3af703cebe8c47f31a7b10f69d3b14f543f90314
SamuelLemosS <107142956+SamuelLemosS@users.noreply.github.com>
2023-06-04 21:37:48 -0300

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,43 @@
+import socket
+import time
+
+HOST = "localhost"
+PORT = 50007
+tipoCliente = input("Você deseja publicar? (Caso não, será um assinante)")
+
+if tipoCliente.lower() == "sim" or tipoCliente.lower() == "s":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.send("pub".encode())
+
+        while True:
+            topico = input("Digite o tópico que deseja enviar (ou 'sair' para encerrar):\n")
+            if topico.lower() == "sair":
+                break
+            s.send(topico.encode())
+
+            mensagem = input("Digite a mensagem que deseja enviar:\n")
+            s.send(mensagem.encode())
+
+        s.send("sair".encode())  
+
+elif tipoCliente.lower() == "nao" or tipoCliente.lower() == "n" or tipoCliente.lower() == "não":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.send("sub".encode())
+
+        topico = input("Digite o tópico que deseja receber:\n")
+        s.send(topico.encode())
+
+        while True:
+            ultimaMensagem = s.recv(1024).decode() #ele nao pega nada quando passa pela segunda vez
+            if ultimaMensagem:
+                print(ultimaMensagem)
+                ultima = ultimaMensagem
+            else:
+                print("Nenhuma nova mensagem")
+
+            time.sleep(1)
+
+else:
+    print("Digite 'sim' ou 'não', por favor.")
---
+++ Server.py
@@ -0,0 +1,53 @@
+import socket
+import threading
+
+HOST = 'localhost'
+PORT = 50007
+topicos = {}
+lock = threading.Lock()
+
+def handle_connection(conn):
+    while True:
+        tipoCliente = conn.recv(1024).decode()
+        if not tipoCliente:
+            break
+        
+        if tipoCliente == "pub":
+            while True:
+                topico = conn.recv(1024).decode()
+                if topico == "sair":
+                    break
+
+                if topico not in topicos:
+                    topicos[topico] = ""
+                print("Novo publisher conectado no tópico:", topico)
+
+                mensagem = conn.recv(1024).decode()
+                topicos[topico] = mensagem
+                print("Nova mensagem do tópico", topico, ":", mensagem)
+        
+        elif tipoCliente == "sub":
+            topico = conn.recv(1024).decode()
+            if topico in topicos:
+                ultimaMensagem = topicos[topico]
+                conn.send(("A última mensagem do topico",topico,"é: ",ultimaMensagem).encode())
+            else:
+                conn.send("Esse tópico não existe ou não possui mensagens".encode())
+            print("Novo subscriber conectado ao tópico", topico)
+
+    conn.close()
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(6)
+        print("Servidor escutando em", HOST + ":" + str(PORT))
+
+        while True:
+            conn, addr = s.accept()
+            print('Conectado em:', addr)
+
+            client_thread = threading.Thread(target=handle_connection, args=(conn,))
+            client_thread.start()
+
+start_server()
